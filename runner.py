85b2192be2242fc0e5ef95aeab7bb232f9bd453b
brookemaeda <bmhm@hawaii.edu>
2024-01-24 16:38:51 -1000

    excludes symbolic links, works in home dir

--- runner.py
+++ runner.py
@@ -5,11 +5,12 @@ from isElf import is_elf_file
 
 count = 0
 for subdir, dirs, files in os.walk("/"):
-    if subdir.startswith(("/usr", "/etc", "/opt", "/root")):
+    if subdir.startswith(("/home", "/usr", "/etc", "/opt", "/root")):
         for file in files:
             filepath = os.path.join(subdir, file)
-            if is_elf_file(filepath):
-                count += 1
+            if os.path.islink(filepath) == False:
+                if is_elf_file(filepath):
+                    count += 1
 
 print(f'Total elf files: {count}')
 
