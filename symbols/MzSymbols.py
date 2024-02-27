fe413789a5e783c3a0a471d69f9ba6d4df0dd671
brookemaeda <bmhm@hawaii.edu>
2024-02-26 21:32:44 -1000

    pe symbol sizes update

--- symbols/MzSymbols.py
+++ symbols/MzSymbols.py
@@ -3,16 +3,36 @@ import pefile
 def findMzSymbols(file_path, debug_mode):
     try:
         pe = pefile.PE(file_path)
+        export_dir_rva = getattr(pe.OPTIONAL_HEADER.DATA_DIRECTORY[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_EXPORT']], 'VirtualAddress', None)
 
-        # Check if the PE file has an export directory
-        if hasattr(pe, 'DIRECTORY_ENTRY_EXPORT'):
-            export_symbols = {}
-            for entry in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-                symbol_name = entry.name.decode('utf-8') if entry.name else None
-                export_symbols[symbol_name] = {
-                    'Ordinal': entry.ordinal,
-                    'Address': entry.address,
-                }
+        if export_dir_rva:
+            export_section = None
+
+            # Find the section containing the export directory
+            for section in pe.sections:
+                if section.VirtualAddress <= export_dir_rva < (section.VirtualAddress + section.Misc_VirtualSize):
+                    export_section = section
+                    break
+            if export_section:
+                export_symbols = {}
+                for idx, entry in enumerate(pe.DIRECTORY_ENTRY_EXPORT.symbols):
+                    symbol_name = entry.name.decode('utf-8') if entry.name else None
+                    symbol_size = 0
+
+                    # Calculate symbol size
+                    if idx < len(pe.DIRECTORY_ENTRY_EXPORT.symbols) - 1:
+                        next_entry = pe.DIRECTORY_ENTRY_EXPORT.symbols[idx + 1]
+                        symbol_size = abs(next_entry.address - entry.address)
+                    else:
+                        # For the last symbol, calculate size from the end of the section
+                        section_end = export_section.VirtualAddress + export_section.Misc_VirtualSize
+                        symbol_size = abs(section_end - entry.address)
+
+                    export_symbols[symbol_name] = {
+                        'Ordinal': entry.ordinal,
+                        'Address': entry.address,
+                        'Size': symbol_size
+                    }
             return export_symbols
         else:
             if debug_mode:
--- symbols/combined_runner.py
+++ symbols/combined_runner.py
@@ -14,7 +14,8 @@ from elfSymbols import findElfSymbols
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='MZ File Analysis Script')
-    parser.add_argument('-debug', type=int, default=0, help='Enable debugging mode (1 for yes, 0 for no)')
+    parser.add_argument('--debug', type=int, default=0, help='Enable debugging mode (1 for yes, 0 for no)')
+    parser.add_argument('--system', required=True, choices=['linux', 'windows'], type=str, default='linux', help='Specify the operating system you are currently on')
     return parser.parse_args()
 
 countElf = 0
@@ -33,58 +34,79 @@ data = []
 args = parse_arguments()
 debug_mode = args.debug
 
-for subdir, dirs, files in os.walk(r"C:\Users\b135c\Downloads"):                                # Change to desired directory on system
-    #if subdir.startswith(("/home", "/usr", "/etc", "/opt", "/root")):      # Comment out this line on Windows
-    for file in files:
-        try:
-            filepath = os.path.join(subdir, file)
-            filename = os.path.basename(filepath)
-            _, extension = os.path.splitext(filename)
-            if os.path.islink(filepath) == False:
-                # Generate information on individual files
-                file_data = 
-                        'Type': '',
-                        'File Path': filepath,
-                        'File Name': filename,
-                        'Extension': extension,
-                        'Date Created': datetime.fromtimestamp(os.path.getctime(filepath)).strftime('%Y-%m-%d %H:%M:%S'),
-                        'File Size': getSize(filepath)
-                        'Symbol Name': '',
-                        'Symbol Size': ''
-                    }
-                count100 += 1
-                if (count100 == 100) and debug_mode == 0:
-                    print(".", end=" ", flush=True)
-                    count100 = 0
-                if isElfFile(filepath):
-                    countElfSize += getSize(filepath)
-                    countElf += 1
-                    symbols = findElfSymbols(filepath)
-                    file_data['Type'] = 'ELF'
-                    if symbols is not None:
-                        for symbol_name, symbol_data in symbols.items():
-                            file_data['Symbol Name'] = symbol_name
-                            file_data['Symbol Size'] = symbol_data['Size']
-                            data.append(file_data.copy())
-                        elfFileInfo.append(file_data)           # Should have an entry for each section in a file
-                elif isMzFile(filepath, debug_mode):
-                    countPeSize += getSize(filepath)
-                    countPE += 1
-                    symbols = findMzSymbols(filepath, debug_mode)
-                    file_data['Type'] = 'PE'
-                    if debug_mode:
-                        print("-" * 50)
-                    # Sum the sizes of each section
-                    if symbols is not None:
-                        for symbol_name, symbol_data in symbols.items():
-                            file_data['Symbol Name'] = symbol_name
-                            file_data['Symbol Size'] = symbol_data['Ordinal']
-                            data.append(file_data.copy())
-                        peFileInfo.append(file_data)                # Should have an entry for each section in a file
-        except Exception as e:
-                print(f"Error accessing file '{filepath}': {e}")
+# Specify filepath(s) to run on based on OS
+if args.system == 'linux':
+    home_directory = r"/"
+    subdirectories = ("/home", "/usr", "/etc", "/opt", "/root")
+elif args.system == 'windows':
+    home_directory = r"C:\Users\b135c\OneDrive\Desktop\MY-MESS\masters\spring\EE699\test"
+    subdirectories = ("")
+for subdir, dirs, files in os.walk(home_directory):
+    if subdir.startswith(subdirectories):
+        for file in files:
+            try:
+                filepath = os.path.join(subdir, file)
+                filename = os.path.basename(filepath)
+                _, extension = os.path.splitext(filename)
+                if os.path.islink(filepath) == False:
+                    # Generate information on individual files
+                    file_data = {
+                            'Type': '',
+                            'File Path': filepath,
+                            'File Name': filename,
+                            'Extension': extension,
+                            'Date Created': datetime.fromtimestamp(os.path.getctime(filepath)).strftime('%Y-%m-%d %H:%M:%S'),
+                            'File Size (Bytes)': getSize(filepath),
+                            'Symbol Name': '',
+                            'Symbol Size (Bytes)': ''
+                        }
+                    count100 += 1
+                    if (count100 == 100) and debug_mode == 0:
+                        print(".", end=" ", flush=True)
+                        count100 = 0
+                    if isElfFile(filepath):
+                        countElfSize += getSize(filepath)
+                        countElf += 1
+                        symbols = findElfSymbols(filepath)
+                        file_data['Type'] = 'ELF'
+                        if symbols is not None:
+                            for symbol_name, symbol_data in symbols.items():
+                                file_data['Symbol Name'] = symbol_name
+                                file_data['Symbol Size'] = symbol_data['Size']
+                                data.append(file_data.copy())
+                            elfFileInfo.append(file_data)           # Should have an entry for each section in a file
+                    elif isMzFile(filepath, debug_mode):
+                        countPeSize += getSize(filepath)
+                        countPE += 1
+                        symbols = findMzSymbols(filepath, debug_mode)
+                        file_data['Type'] = 'PE'
+                        if debug_mode:
+                            print("-" * 50)
+                        # Sum the sizes of each section
+                        if symbols is not None:
+                            for symbol_name, symbol_data in symbols.items():
+                                file_data['Symbol Name'] = symbol_name
+                                file_data['Symbol Size'] = symbol_data['Size']
+                                data.append(file_data.copy())
+                            peFileInfo.append(file_data)                # Should have an entry for each section in a file
+            except Exception as e:
+                    print(f"Error accessing file '{filepath}': {e}")
 # Create DataFrame from the collected data
 df = pd.DataFrame(data)
 
-# Save DataFrame to CSV
-df.to_csv('combined_symbol_results.csv', index=False)
+if args.system == 'linux':
+    # Save DataFrame to CSV and txt
+    df.to_csv('results/linux/combined_symbol_results.csv', index=False)
+    df.to_csv('results/linux/combined_symbol_results.txt', sep='\t', index=False)
+elif args.system == 'windows':
+    # Save DataFrame to CSV and txt
+    df.to_csv('results/windows/combined_symbol_results.csv', index=False)
+    df.to_csv('results/windows/combined_symbol_results.txt', sep='\t', index=False)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"\nTotal ELF files: {countElf}")
+print(f"Total ELF file size: {countElfSize} bytes\n")
+print(f"\nTotal PE files: {countPE}")
+print(f"Total PE file size: {countPeSize} bytes\n")
+print(f"Total runtime: {elapsed_time:.2f} seconds\n")
