e589be324d7e553b95dfa0ccb230a35af76d07c6
brookemaeda <bmhm@hawaii.edu>
2024-03-01 22:34:31 -1000

    added debug mode, fixed count

--- segment_runner.py
+++ segment_runner.py
@@ -3,6 +3,7 @@ import os
 import time
 import platform
 import math
+import argparse
 from isElf import isElfFile
 from isMZ import isMzFile
 import pefile
@@ -21,6 +22,13 @@ peFileInfo = []
 start_time = time.time()
 
 current_system = platform.system()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='File Segment Analysis Script')
+    parser.add_argument('--debug', type=int, default=0, help='Enable debugging mode (1 for yes, 0 for no)')
+    return parser.parse_args()
+args = parse_arguments()
+debug_mode = args.debug
 
 # Specify filepath(s) to run on based on OS
 if current_system == 'Linux':
@@ -47,18 +55,20 @@ for subdir, dirs, files in os.walk(home_directory):
                         count100 = 0
                     if isElfFile(filepath):
                         countElf += 1
-                        print("\nELF file:", filepath, "\n----------------------------")
+                        if debug_mode == 1:
+                            print("\nELF file:", filepath, "\n----------------------------")
                         with open(filepath, 'rb') as f:
                             elf = ELFFile(f)
                             for segment in elf.iter_segments():
-                                print("Segment type:", segment['p_type'])
-                                print("Permissions:", describe_p_flags(segment['p_flags']))
-                                print("Virtual address:", hex(segment['p_vaddr']))
-                                print("Physical address:", hex(segment['p_paddr']))
-                                print("File offset:", hex(segment['p_offset']))
-                                print("Size in memory:", segment['p_memsz'])
-                                print("Size in file:", segment['p_filesz'])
-                                print()
+                                if debug_mode == 1:
+                                    print("Segment type:", segment['p_type'])
+                                    print("Permissions:", describe_p_flags(segment['p_flags']))
+                                    print("Virtual address:", hex(segment['p_vaddr']))
+                                    print("Physical address:", hex(segment['p_paddr']))
+                                    print("File offset:", hex(segment['p_offset']))
+                                    print("Size in memory:", segment['p_memsz'])
+                                    print("Size in file:", segment['p_filesz'])
+                                    print()
 
                                 name = segment['p_type']
                                 flags = describe_p_flags(segment['p_flags'])
@@ -92,15 +102,16 @@ for subdir, dirs, files in os.walk(home_directory):
 
                     elif isMzFile(filepath, 0):
                         countPE += 1
-                        print("\nPE file:", filepath, "\n----------------------------")
+                        if debug_mode == 1:
+                            print("\nPE file:", filepath, "\n----------------------------")
                         pe = pefile.PE(filepath)
                         for section in pe.sections:
                             name = section.Name.decode().rstrip('\x00')  # Clean up section name
                             characteristics = section.Characteristics
                             size_of_raw_data = section.SizeOfRawData
                             key = (name, characteristics)
-                            print(section)
-                            print(key)
+                            if debug_mode == 1:
+                                print(section)
 
                             if key not in peSectionSizeData:
                                 peSectionSizeData[key] = {
@@ -120,7 +131,7 @@ for subdir, dirs, files in os.walk(home_directory):
                 print(f"Error accessing file '{filepath}': {e}")
 
 print("\nPE File Segment Analysis:\n")
-print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format("Segment Name", "Characteristics", "Avg Size of Raw Data (bytes)", "Max", "Min", "STD", "Count"))
+print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format("Segment Name", "Characteristics", "Avg Size of Raw Data (bytes)", "Max", "Min", "STD", "Count"))
 for key, data in peSectionSizeData.items():
     name = key[0]
     characteristics = key[1]
@@ -128,10 +139,10 @@ for key, data in peSectionSizeData.items():
     avg = data["sum"] / count
     variance = sum((x - avg) ** 2 for x in data["sizes"]) / count
     std_dev = math.sqrt(variance)
-    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format(name, hex(characteristics), round(avg, 2), data['max'], data['min'], round(std_dev, 2), count))
+    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format(name, hex(characteristics), round(avg, 2), data['max'], data['min'], round(std_dev, 2), count))
 
 print("\nELF File Segment Analysis (Sizes in Memory):\n")
-print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format("Segment Name", "Characteristics", "Avg Size (bytes)", "Max", "Min", "STD", "Count"))
+print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format("Segment Name", "Characteristics", "Avg Size (bytes)", "Max", "Min", "STD", "Count"))
 for key, data in elfSectionSizeData.items():
     name = key[0]
     flags = key[1]
@@ -139,10 +150,10 @@ for key, data in elfSectionSizeData.items():
     avg = data["suminMem"] / count
     variance = sum((x - avg) ** 2 for x in data["sizesInMem"]) / count
     std_dev = math.sqrt(variance)
-    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format(name, flags, round(avg, 2), data['maxInMem'], data['minInMem'], round(std_dev, 2), count))
+    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format(name, flags, round(avg, 2), data['maxInMem'], data['minInMem'], round(std_dev, 2), count))
 
 print("\nELF File Segment Analysis (Sizes in File):\n")
-print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format("Segment Name", "Characteristics", "Avg Size (bytes)", "Max", "Min", "STD", "Count"))
+print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format("Segment Name", "Characteristics", "Avg Size (bytes)", "Max", "Min", "STD", "Count"))
 for key, data in elfSectionSizeData.items():
     name = key[0]
     flags = key[1]
@@ -150,7 +161,7 @@ for key, data in elfSectionSizeData.items():
     avg = data["suminFile"] / count
     variance = sum((x - avg) ** 2 for x in data["sizesInFile"]) / count
     std_dev = math.sqrt(variance)
-    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<8}".format(name, flags, round(avg, 2), data['maxInFile'], data['minInFile'], round(std_dev, 2), count))
+    print("{:<20} {:<20} {:<30} {:<10} {:<15} {:<20} {:<8}".format(name, flags, round(avg, 2), data['maxInFile'], data['minInFile'], round(std_dev, 2), count))
 
 
 end_time = time.time()
